97a3fb392fa1f51c31b55c81db68c195924c2c41
aneesh_augustine <aneeshaugustine911@gmail.com>
2023-07-23 16:48:38 +0530

    remove buttin added

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ function App() {
           placeholder="🖊️ Add item..."
         />
         <i
-          onClick={() => setToDos([...toDos,{id: Date.now(),text: toDo, status:false}])}
+          onClick={() =>
+            setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])
+          }
           className="fas fa-plus"
         ></i>
       </div>
@@ -30,14 +32,47 @@ function App() {
           return (
             <div className="todo">
               <div className="left">
-                <input value={obj.status} type="checkbox" name="" id="" />
+                <input
+                  onChange={(e) => {
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
                 <p>{obj.text}</p>
               </div>
               <div className="right">
-                <i className="fas fa-times"></i>
+                <i
+                  onClick={(e) => {
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2=null;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  className="fas fa-times"
+                ></i>
               </div>
             </div>
           );
+        })}
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return <h1>{obj.text}</h1>;
+          }
+          return null;
         })}
       </div>
     </div>
